7de6637279e07d9daeeeb50f5aae903cd4bfd0f7
banothsidhu <sidhubanoth01@gmail.com>
2023-04-29 18:21:57 +0530

    till getuser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const fetchuser = require('../middleware/fetchuser');
 const JWT_TOKEN = 'This Is The JWT Token';
 
-// Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+//SignUp User (/api/auth/createuser)------------------------------------------
 router.post(
     '/createuser',
     [
@@ -51,5 +51,59 @@ router.post(
         }
     }
 );
+
+
+//Login (/api/auth/Login)------------------------------------------
+
+router.post(
+    '/login',
+    [
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', "Pasword cant be blank").exists(),
+    ],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ msg: 'Invalid Credentials' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ msg: 'Invalid Credentials' });
+            }
+
+            const authToken = jwt.sign({ id: user._id }, JWT_TOKEN);
+            res.json(authToken);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
+//Logged in user Details ----------------------------
+
+
+router.post('/getuser',fetchuser, async (req, res) => {
+    try {
+        const userId = req.user.id
+        const user = await User.findById(userId).select("-password")
+        try {
+            res.send(user)
+        } catch (e) {
+            res.json("No User found")
+        }
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 module.exports = router;
