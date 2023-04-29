d417822b22085055c0b3088fedfb62f02688aa12
banothsidhu <sidhubanoth01@gmail.com>
2023-04-29 13:20:26 +0530

    Till_Jwt_token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,24 +2,54 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_TOKEN = 'This Is The JWT Token';
 
 // Create a User using: POST "/api/auth/createuser". Doesn't require Auth
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+router.post(
+    '/createuser',
+    [
+        body('name', 'Enter a valid name').isLength({ min: 3 }),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let user = await User.findOne({ email: req.body.email });
+
+        try {
+            if (user) {
+                return res.status(400).json('User Already Exists');
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                console.log(salt);
+                const secPass = bcrypt.hashSync(req.body.password);
+
+                let user = await User.create({
+                    name: req.body.name,
+                    password: secPass,
+                    email: req.body.email,
+                });
+                try {
+                    const authToken = jwt.sign({ id: user._id }, JWT_TOKEN);
+                    res.json(authToken)
+
+
+                } catch (err) {
+                    res.status(400).json('Something went wrong');
+                }
+            }
+        } catch (err) {
+            res.status(400).json('Something went wrong');
+        }
     }
-    let user = await User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    })
+);
 
-})
-
-module.exports = router
+module.exports = router;
