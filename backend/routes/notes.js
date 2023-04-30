2c48ecc7518481105fba673223af39585ce0fee2
banothsidhu <sidhubanoth01@gmail.com>
2023-04-30 10:01:46 +0530

    UpdateNote Completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,5 +45,36 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+// Updating the existing notes --------------------------------
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body
+        const newNote = {}
+
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag }
+
+        let note = await Note.findById(req.params.id)
+
+        if (!note) {
+            return res.status(404).json("No Note Found!")
+        }
+        console.log(note)
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).json("Not Allowed!")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json(note)
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+})
+
+
 
 module.exports = router
