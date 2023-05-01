67229a69af4e5e7b2ead227a4aef0efdcb976721
banothsidhu <sidhubanoth01@gmail.com>
2023-05-01 18:49:58 +0530

    modified styles. Next work to start Edit Note.

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,40 +1,47 @@
 import React, { useContext, useState } from 'react';
 import noteContext from '../context/Notes/NoteContext';
 import showToast from './Toastify';
+
 const AddNote = () => {
-    const context = useContext(noteContext)
-    const { addNote, notes } = context
-    const [note, setNote] = useState({ title: "", description: "", tag: "default" })
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({ title: '', description: '', tag: 'General' });
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        if (note.title.length >= 3 && note.description.length >= 3) {
-            addNote(note.title, note.description)
-        } else {
-            showToast("error","Title or Description must be at least 3 characters")
-        }
-    };
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (note.title.length >= 3 && note.description.length >= 3) {
+      addNote(note.title, note.description, note.tag);
+    } else {
+      showToast('error', 'Title or Description must be at least 3 characters');
+    }
+  };
 
-    const onChange = (e) => {
-        setNote({ ...note, [e.target.name]: e.target.value })
-    };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div className='m-4'>
-            <h2>Add A Note</h2>
-            <form className='my-4'>
-                <div className="form-group my-3">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter Title" onChange={onChange} name='title' value={note.title} />
-                </div>
-                <div className="form-group my-3">
-                    <label htmlFor="description">Description</label>
-                    <input type="text" className="form-control" id="description" placeholder="Enter Description" onChange={onChange} name='description' value={note.description} />
-                </div>
-                <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
-            </form>
+  return (
+    <div className="container my-3">
+      <h2>Add A Note</h2>
+      <form className="my-4">
+        <div className="form-group my-3">
+          <label htmlFor="title">Title</label>
+          <input type="text" className="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter Title" onChange={onChange} name="title" value={note.title} />
         </div>
-    )
-}
+        <div className="form-group my-3">
+          <label htmlFor="description">Description</label>
+          <textarea className="form-control" id="description" placeholder="Enter Description" onChange={onChange} name="description" value={note.description} rows="5" cols="40"></textarea>
+        </div>
+        <div className="form-group my-3">
+          <label htmlFor="tag">Tag</label>
+          <input type="text" className="form-control" id="tag" placeholder="Enter Tag" onChange={onChange} name="tag" value={note.tag} />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default AddNote
+export default AddNote;
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,7 +6,7 @@ import noteContext from '../context/Notes/NoteContext';
 const NoteItem = (props) => {
   const context = useContext(noteContext)
   const { note } = props;
-  const {deleteNote} = context;
+  const { deleteNote } = context;
 
   const handleShowToast = (type, message) => () => {
     showToast(type, message);
@@ -14,15 +14,28 @@ const NoteItem = (props) => {
 
   return (
     <>
-      <div className='col-sm-6 col-md-4 col-lg-3 mx-auto my-2'>
-        <div className="card" style={{ width: 'auto', minWidth: "12rem", padding: '0.3rem' }}>
-          <div className="card-body">
+      <div className='col-sm-8 col-md-6 col-lg-4 mx-auto my-2'>
+        <div className="card" style={{ width: 'auto', minWidth: '20rem', padding: '0.3rem', boxShadow: '0 2px 8px rgba(0,0,0,0.3)', borderRadius: '5px', backgroundColor: 'white' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,flexDirection:"column" }}>
+          <span className='Center  font-weight-bold' style={{ display: 'inline-block', width: '50%', textAlign: 'center', borderRadius: "5px", backgroundColor: "#777777", padding: '0.2rem',color:"green" ,fontSize:"20px" }}>
+              
+            </span>
+            {note.tag !== '' ? 
+              <span className='Center  font-weight-bold my-3' style={{ display: 'inline-block', width: '40%', textAlign: 'center', borderRadius: "5px", backgroundColor: "#E1FFF0", padding: '0.2rem',color:"green" ,fontSize:"20px" }}>
+                {note.tag}
+              </span> :
+              null
+            }
+          </div>
+          <div className="card-body d-flex flex-column">
             <div className="d-flex align-items-center">
-              <h5 className="card-title">{note.title}</h5>
-              <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}} ></i>
-              <i className="fa-solid fa-edit mx-2"  ></i>
+              <h5 className="card-title"><strong>{note.title}</strong></h5>
+              <div className='ml-auto'>
+                <i className="fa-solid fa-trash mx-2" onClick={() => { deleteNote(note._id) }} ></i>
+                <i className="fa-solid fa-edit mx-2"></i>
+              </div>
             </div>
-            <p className="card-text">The Description of this note is {note.description}</p>
+            <p className="card-text">{note.description}</p>
           </div>
         </div>
       </div>
