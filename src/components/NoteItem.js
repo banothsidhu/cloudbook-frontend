68c832bd08d72a00e2e90dbc3853acc76f4e1ce7
banothsidhu <sidhubanoth207@gmail.com>
2023-05-02 08:24:48 +0530

    From Old

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -17,9 +17,9 @@ const NoteItem = (props) => {
       <div className='col-sm-8 col-md-6 col-lg-4 mx-auto my-2'>
         <div className="card" style={{ width: 'auto', minWidth: '20rem', padding: '0.3rem', boxShadow: '0 2px 8px rgba(0,0,0,0.3)', borderRadius: '5px', backgroundColor: 'white' }}>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,flexDirection:"column" }}>
-          <span className='Center  font-weight-bold' style={{ display: 'inline-block', width: '50%', textAlign: 'center', borderRadius: "5px", backgroundColor: "#777777", padding: '0.2rem',color:"green" ,fontSize:"20px" }}>
+          {/* <span className='Center  font-weight-bold' style={{ display: 'inline-block', width: '50%', textAlign: 'center', borderRadius: "5px", backgroundColor: "#777777", padding: '0.2rem',color:"green" ,fontSize:"20px" }}>
               
-            </span>
+            </span> */}
             {note.tag !== '' ? 
               <span className='Center  font-weight-bold my-3' style={{ display: 'inline-block', width: '40%', textAlign: 'center', borderRadius: "5px", backgroundColor: "#E1FFF0", padding: '0.2rem',color:"green" ,fontSize:"20px" }}>
                 {note.tag}
